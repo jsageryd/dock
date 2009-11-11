68f08c2f6f25254fa19c0abf46add6ba1964e79d
Johan Sageryd <j416@1616.se>
2009-11-11 11:52:51 +0900

    Add autohide; fix tilesize

--- fixdock.bash
+++ fixdock.bash
@@ -4,6 +4,7 @@
 
 defaults write com.apple.dock pinning -string end
 defaults write com.apple.dock orientation -string left
-defaults write com.apple.dock tilesize -int 25
+defaults write com.apple.dock tilesize -int 23
+defaults write com.apple.dock autohide -bool YES
 
 killall Dock
--- restoredock.bash
+++ restoredock.bash
@@ -5,5 +5,6 @@
 defaults delete com.apple.dock pinning
 defaults delete com.apple.dock orientation
 defaults delete com.apple.dock tilesize
+defaults delete com.apple.dock autohide
 
 killall Dock
