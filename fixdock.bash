2e1b01ac8351fd5924357eee8ac352d16af16039
Johan Sageryd <j416@1616.se>
2009-11-11 13:06:56 +0900

    Use new list stack

--- fixdock.bash
+++ fixdock.bash
@@ -6,5 +6,6 @@ defaults write com.apple.dock pinning -string end
 defaults write com.apple.dock orientation -string left
 defaults write com.apple.dock tilesize -int 23
 defaults write com.apple.dock autohide -bool YES
+defaults write com.apple.dock use-new-list-stack -bool YES
 
 killall Dock
--- restoredock.bash
+++ restoredock.bash
@@ -6,5 +6,6 @@ defaults delete com.apple.dock pinning
 defaults delete com.apple.dock orientation
 defaults delete com.apple.dock tilesize
 defaults delete com.apple.dock autohide
+defaults delete com.apple.dock use-new-list-stack
 
 killall Dock
