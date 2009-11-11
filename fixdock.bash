b5aa83e691a2a79b367d722baddd2f726c16022d
Johan Sageryd <j416@1616.se>
2009-11-11 23:41:56 +0900

    Remove autohide

--- fixdock.bash
+++ fixdock.bash
@@ -5,7 +5,6 @@
 defaults write com.apple.dock pinning -string end
 defaults write com.apple.dock orientation -string left
 defaults write com.apple.dock tilesize -int 23
-defaults write com.apple.dock autohide -bool YES
 defaults write com.apple.dock use-new-list-stack -bool YES
 defaults write com.apple.dock mouse-over-hilite-stack -boolean YES
 
