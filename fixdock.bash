a976f79734a9420dd34612cda6741bfe169b998f
Johan Sageryd <j416@1616.se>
2009-11-11 13:14:23 +0900

    Use mouse-over highlight for stacks

--- fixdock.bash
+++ fixdock.bash
@@ -7,5 +7,6 @@ defaults write com.apple.dock orientation -string left
 defaults write com.apple.dock tilesize -int 23
 defaults write com.apple.dock autohide -bool YES
 defaults write com.apple.dock use-new-list-stack -bool YES
+defaults write com.apple.dock mouse-over-hilite-stack -boolean YES
 
 killall Dock
--- restoredock.bash
+++ restoredock.bash
@@ -7,5 +7,6 @@ defaults delete com.apple.dock orientation
 defaults delete com.apple.dock tilesize
 defaults delete com.apple.dock autohide
 defaults delete com.apple.dock use-new-list-stack
+defaults delete com.apple.dock mouse-over-hilite-stack
 
 killall Dock
